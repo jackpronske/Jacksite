420e14bad5c3b168ca76275b2176f5313033802c
Jack <jackpronske@yahoo.com>
2021-06-18 18:41:47 -0700

    --implemented the center div to move to the left and hide in the frame div, I might revisit this later, becuase maybe I want a new style of site later

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,12 +3,35 @@ import React from 'react';
 class Background extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      x: 0,
+    };
+    this.moveLeft = this.moveLeft.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(this.moveLeft, 50);
+  }
+
+  moveLeft() {
+    const { x } = this.state;
+    const newX = x - 5;
+    this.setState({
+      x: newX,
+    })
   }
 
   render() {
+    const { x } = this.state;
     return (
-      <div id="background-rectangle"></div>
+      <div id="background-frame">
+        <div
+          id="background-rectangle"
+          style={{
+            transform: `translateX(${x}px)`
+          }}
+        ></div>
+      </div>
     )
   }
 }
