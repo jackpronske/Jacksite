180e9254dc2701ea087fcdc155a7ca3e4f294dbd
Jack <jackpronske@yahoo.com>
2021-06-22 15:02:01 -0700

    --implemented webpack and babel again

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
