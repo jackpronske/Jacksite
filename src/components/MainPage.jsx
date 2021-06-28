11ee2f7115d9e0d3f888a8e6b678b0dca2274365
Jack <jackpronske@yahoo.com>
2021-06-28 09:49:12 -0700

    added some template information for the mainpage, needs to be cleaned up and boxed out with borders and general wireframe needs to be created

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -8,7 +8,20 @@ class MainPage extends Component {
 
   render() {
     return (
-      <div> Main Page </div>
+      <div>
+        <div> Jack Pronske </div>
+        <ul>
+          <li>Home</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+        <div>Blog Stuff? Maybe some dev twitter posts, screen shots</div>
+        <h3>Work</h3>
+        <div>Work examples, starting with FEC gif, create the breathe page</div>
+        <h3>Contact</h3>
+        <div>this should be at the bottom of the page, my contact info and possibly a way to copy my email address from a click of a buton</div>
+        <div>social media icons (linkedin, dev twitter, github)</div>
+      </div>
     );
   }
 };
