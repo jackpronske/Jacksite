8b2a46baf8bcc004536c5b8d05e63d7e3390fb47
Jack <jackpronske@yahoo.com>
2021-06-28 14:33:28 -0700

    got a start to the enter site page with good css animations matching what I had in my head

--- src/components/EnterSite.jsx
+++ src/components/EnterSite.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 const EnterSite = (props) => (
   <div className="enter-site-container">
     <div className="welcome-track">
-      <div className="welcome-slider">welcome to jack pronske</div>
+      <div className="welcome-slider">welcome to <em>jack pronske</em></div>
     </div>
     <div
       className="enter-site-button"
       onClick={() => props.showHide('showEnterSite')}
-    >enter site</div>
+    ></div>
   </div>
 );
 
