6280d38a2ef8e8b51a7dfb0808e36d293cd3779d
Jack <jackpronske@yahoo.com>
2021-06-28 16:12:56 -0700

    enter site page complete for now

--- src/components/EnterSite.jsx
+++ src/components/EnterSite.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const EnterSite = (props) => (
   <div className="enter-site-container">
     <div className="welcome-track">
-      <div className="welcome-slider">welcome to jack pronske</div>
+      <div className="welcome-slider" id="transparent-text">welcome to</div>
+    </div>
+    <div className="welcome-track">
+      <div className="welcome-slider"> jack pronske</div>
     </div>
     <div
       className="enter-site-button"
