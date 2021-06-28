e5382afe742a4b2a4a154fa106b8d3966926b9da
Jack <jackpronske@yahoo.com>
2021-06-28 14:53:28 -0700

    enough with this, need to start on wireframe of main page

--- src/components/EnterSite.jsx
+++ src/components/EnterSite.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const EnterSite = (props) => (
   <div className="enter-site-container">
     <div className="welcome-track">
-      <div className="welcome-slider">welcome to <em>jack pronske</em></div>
+      <div className="welcome-slider">welcome to jack pronske</div>
     </div>
     <div
       className="enter-site-button"
