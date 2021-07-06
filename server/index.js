60484caee5f6689e14f082c43ebea96f30c2bb59
Jack <jopronske@gmail.com>
2021-07-05 17:28:42 -0700

    added pathing for the express server to server up our index.html file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.listen(3000, () => {
   console.log('listening on localhost:3000');
