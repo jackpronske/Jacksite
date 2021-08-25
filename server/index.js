28ef17f85b00db63ae84af27e5bdb10ec141c8d1
Jack <jopronske@gmail.com>
2021-08-25 14:56:33 -0500

    added a process env to the server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("listening on localhost:3000");
 });
