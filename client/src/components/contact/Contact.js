18639f0827a6ac2b384aaaa9b4ac1ae16b40cebc
Jack <jopronske@gmail.com>
2021-08-24 19:51:16 -0500

    added links and some minor css to the contacts section, still needs touch ups

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -6,24 +6,29 @@ const Contact = () => {
     {
       name: "Jopronske@gmail.com",
       image_url: "images/Mail.png",
+      type: "MAIL",
     },
     {
       name: "Austin, TX",
       image_url: "images/Location.png",
+      type: "LOCATION",
     },
     {
       name: "Phone | Zoom",
       image_url: "images/Phone.png",
+      type: "PHONE",
     },
     {
       name: "GitHub",
       image_url: "images/GitHub.png",
       url: "https://github.com/jackpronske",
+      type: "GITHUB",
     },
     {
       name: "LinkedIn",
       image_url: "images/LinkedIn.png",
       url: "https://www.linkedin.com/in/jackpronske/",
+      type: "LINKEDIN",
     },
   ];
   return (
--- client/src/components/contact/ContactItem.js
+++ client/src/components/contact/ContactItem.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 const ContactItem = ({ contact }) => {
+  const handleClick = () => {
+    switch (contact.type) {
+      case "MAIL":
+        navigator.clipboard.writeText(contact.name);
+      case "LOCATION":
+        window.open("https://youtu.be/AW2cmIIomac?t=246", "_blank");
+      case "PHONE":
+        window.open("https://zoom.us/", "_blank");
+      case "GITHUB":
+        window.open(contact.url, "_blank");
+      case "LINKEDIN":
+        window.open(contact.url, "_blank");
+    }
+  };
   return (
-    <div className="contact-item-container">
+    <div
+      onClick={() => {
+        handleClick();
+      }}
+      className="contact-item-container"
+    >
       <img className="contact-item-image" src={contact.image_url} />
       <p className="contact-item-name">{contact.name}</p>
     </div>
