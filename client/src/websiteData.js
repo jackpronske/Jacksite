71e5b0d56740096353aeabc052c5f123e3b83a03
Jack <jopronske@gmail.com>
2021-08-26 19:37:44 -0500

    added a website data file that cleans up code readability

---
+++ client/src/websiteData.js
@@ -0,0 +1,33 @@
+const contactObjects = [
+  {
+    name: "Jopronske@gmail.com",
+    image_url: "images/Mail.png",
+    type: "MAIL",
+  },
+  {
+    name: "Austin, TX",
+    image_url: "images/Location.png",
+    type: "LOCATION",
+  },
+  {
+    name: "Phone | Zoom",
+    image_url: "images/Phone.png",
+    type: "PHONE",
+  },
+  {
+    name: "GitHub",
+    image_url: "images/GitHub.png",
+    url: "https://github.com/jackpronske",
+    type: "GITHUB",
+  },
+  {
+    name: "LinkedIn",
+    image_url: "images/LinkedIn.png",
+    url: "https://www.linkedin.com/in/jackpronske/",
+    type: "LINKEDIN",
+  },
+];
+
+module.exports = {
+  contactObjects,
+};
